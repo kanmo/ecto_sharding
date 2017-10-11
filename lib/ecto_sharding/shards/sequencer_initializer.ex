18fcd6dce127c6234d408c964763242d46b258bb
kanmo <kanmo.ak@gmail.com>
2017-10-12 06:49:22 +0900

    Refactoring and add readme

---
+++ lib/ecto_sharding/shards/sequencer_initializer.ex
@@ -0,0 +1,46 @@
+defmodule Ecto.Sharding.Shards.SequencerInitializer do
+  defmacro __using__(_) do
+    quote do
+      import Ecto.Sharding.Shards
+      import Logger
+      import Ecto.Query
+
+      @app_name Application.get_env(:ecto_sharding, :config)[:app_name]
+      @base_module_name Application.get_env(:ecto_sharding, :config)[:base_module_name]
+      @sequencer_config Application.get_env(@app_name, :sequencer)
+
+      def create_sequencer_module do
+        db = @sequencer_config[:sequencer]
+        mod = sequencer_module_name(@base_module_name, Sequencer)
+        Application.put_env(@app_name, mod, db)
+
+        Ecto.Sharding.Shards.create_sequencer_module(%{table: @sequencer_config[:table], app_name: @app_name, module: mod})
+
+        ecto_repos = Application.get_env(@app_name, :ecto_repos)
+        Application.put_env(@app_name, :ecto_repos, ecto_repos ++ [mod])
+
+        mod
+      end
+
+      def generate_sequencer_repository_supervisor(children) do
+        import Supervisor.Spec, warn: false
+
+        children ++ [
+          supervisor(Ecto.Sharding.Repositories.SequencerSupervisor,
+            [%{ worker_name: @sequencer_config[:worker_name],
+                utils: __MODULE__,
+                name: @sequencer_config[:supervisor_name]}],
+            [id: make_ref()])
+        ]
+      end
+
+      def next_sequence_id do
+        update_query = "UPDATE `#{@cluster_config[:table]}` SET id = LAST_INSERT_ID(id + 1)"
+        mod = sequencer_module_name(@base_module_name, Sequencer)
+        resp = mod.run(update_query)
+
+        resp.last_insert_id
+      end
+    end
+  end
+end
---
+++ lib/ecto_sharding/shards/sharding_initializer.ex
@@ -0,0 +1,73 @@
+defmodule Ecto.Sharding.Shards.ShardingInitializer do
+  defmacro __using__(_) do
+    quote do
+      import Ecto.Sharding.Shards
+      import Logger
+      import Ecto.Query
+
+      @app_name Application.get_env(:ecto_sharding, :config)[:app_name]
+      @base_module_name Application.get_env(:ecto_sharding, :config)[:base_module_name]
+      @cluster_config Application.get_env(@app_name, :cluster)
+
+      unless @cluster_config[:databases] do
+        raise "Config cluster databases can't be nil"
+      end
+
+      def repositories_to_load do
+        for n <- 0..(@cluster_config[:count] - 1) do
+          __MODULE__.create_repository_module(n)
+        end
+      end
+
+      def create_repository_module(position) do
+        db = Enum.at(@cluster_config[:databases], position)
+        mod = repository_module(position)
+        Application.put_env(@app_name, mod, db)
+
+        Ecto.Sharding.Shards.create_repository_module(%{position: position, table: @cluster_config[:table_name], app_name: @app_name, module: mod})
+
+        ecto_repos = Application.get_env(@app_name, :ecto_repos)
+        Application.put_env(@app_name, :ecto_repos, ecto_repos ++ [mod])
+
+        mod
+      end
+
+      def repository_module(position) do
+        repository_module_name(@base_module_name, @cluster_config[:name], position)
+      end
+
+      def generate_sharding_repository_supervisor(children) do
+        count = Application.get_env(:sharding_app, :cluster)[:count]
+
+        if count && count > 0 do
+          import Supervisor.Spec, warn: false
+
+          children ++ [
+            supervisor(Ecto.Sharding.Repositories.ShardedSupervisor,
+              [%{ worker_name: @cluster_config[:worker_name],
+                  utils: __MODULE__,
+                  name: @cluster_config[:supervisor_name]}],
+              [id: make_ref()])
+          ]
+        else
+          children
+        end
+      end
+
+      def sharded_insert(changeset) do
+        id = next_sequence_id
+        position = shard_for(@cluster_config[:count], id)
+        sharded_changeset = Ecto.Changeset.change(changeset, %{user_id: id})
+        insert_all(position, @cluster_config[:table], [sharded_changeset.changes])
+      end
+
+      def insert_all(position, table_name, changeset) when is_list(changeset) do
+        repository_module_name(@base_module_name, @cluster_config[:name], position).insert_all(table_name, changeset, [])
+      end
+
+      def insert_all(position, table_name, changeset) do
+        insert_all(position, table_name, [changeset])
+      end
+    end
+  end
+end
